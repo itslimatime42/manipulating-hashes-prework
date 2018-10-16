56ff02db61d08760ac88eef3113b22d0138dad57
Robert Schulte <schulte.robertw@outlook.com>
2018-10-16 18:02:57 -0500

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -6,8 +8,8 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  #code your solution here!
-
-  
+  groceries.values.flatten
 
 end
+
+# Use the .values method to collect all of the values of the grocery type keys (:dairy, :vegetables, :meat, :grains). The method should return a one-dimensional (or "flat") array that only includes the values (groceries such as "milk" and "carrots") without their keys.
